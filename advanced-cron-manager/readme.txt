b187300e7508bc02c81ea890cef7ea74532491a0
Sean Murphy <sean@iamseanmurphy.com>
2025-08-31 12:19:50 +0000

    2.6.2

--- advanced-cron-manager/readme.txt
+++ advanced-cron-manager/readme.txt
@@ -4,7 +4,7 @@ Tags: cron, wpcron, tool, manager, crontrol
 Requires at least: 3.6
 Requires PHP: 5.3
 Tested up to: 6.8
-Stable tag: 2.6.0
+Stable tag: 2.6.2
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -100,6 +100,13 @@ Yes! We're offering a [custom plugin development](https://bracketspace.com/custo
 6. Server Scheduler section
 
 == Changelog ==
+
+= 2.6.2 =
+* [Fixed] Fixed stable package creation issues - moved package exclusions to .distignore and ensured vendor and assets directories are included.
+
+= 2.6.1 =
+* [Changed] Exclude Claude configuration files from plugin distribution exports.
+* [Changed] Add composer and yarn build steps to stable release workflow.
 
 = 2.6.0 =
 * [Changed] Removed textdomain loading to fix WordPress 6.7+ localization warnings - translations now load automatically from GlotPress.
--- advanced-cron-manager/vendor/composer/installed.php
+++ advanced-cron-manager/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'bracketspace/advanced-cron-manager',
-        'pretty_version' => '2.5.10',
-        'version' => '2.5.10.0',
-        'reference' => 'ea164a52de8deea8ec8ae6072933a6df911c8c84',
+        'pretty_version' => '2.6.2',
+        'version' => '2.6.2.0',
+        'reference' => '02c56171f0ac7231fe8d826e5df08b5a4ca597c8',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'bracketspace/advanced-cron-manager' => array(
-            'pretty_version' => '2.5.10',
-            'version' => '2.5.10.0',
-            'reference' => 'ea164a52de8deea8ec8ae6072933a6df911c8c84',
+            'pretty_version' => '2.6.2',
+            'version' => '2.6.2.0',
+            'reference' => '02c56171f0ac7231fe8d826e5df08b5a4ca597c8',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
